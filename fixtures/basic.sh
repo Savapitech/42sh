f207ad578c1ffbc52f5c2d6b632af6dc0727fa80
savalet <savinien.petitjean@gmail.com>
2025-02-19 19:18:17 +0100

    Add basic tests

---
+++ fixtures/basic.sh
@@ -0,0 +1,47 @@
+ls /etc
+cat /etc/resolv.conf
+pwd
+pwd
+cat /etc/resolv.conf
+sleep 0.5
+ls -l /etc
+sleep 0.5
+pwd
+unsetenv HOSTTYPE
+unsetenv VENDOR
+unsetenv OSTYPE
+unsetenv MACHTYPE
+unsetenv SHLVL
+unsetenv PWD
+unsetenv LOGNAME
+unsetenv USER
+unsetenv GROUP
+unsetenv HOST
+unsetenv REMOTEHOST
+unsetenv _
+unsetenv LS_COLORS
+unsetenv NOTE
+unsetenv
+setenv titi
+setenv tata coucou
+env
+/usr/bin/env
+setenv var1 val1
+setenv var2 val2
+setenv pouet pouet pouet
+setenv
+setenv @
+setenv a-
+cd ..
+ls -l
+cd /usr
+ls -l
+builtins
+:
+cd pouet
+cd test
+cd /root
+cd
+cd ~
+cd pouet1 pouet2
+exit
