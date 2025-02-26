907dcf3ea454e8f784eebf0df58982c291d3b499
savalet <savinien.petitjean@gmail.com>
2025-02-26 23:19:25 +0100

    Add best cov tests
    fdglkj
    
    Add best cov

---
+++ fixtures/commands.sh
@@ -0,0 +1,52 @@
+setenv HOME /tmp
+cd /
+ls -d
+cd -
+ls -d
+cd ~
+ls -d
+cd
+cd ..
+cd .
+cd /this/should/not/exist
+cd -
+cd ~
+cd
+cd /bin/sh
+cd /root
+cd test test
+qWeRtY -k
+env A=1 -i ls
+env -h
+env -j
+setenv hello 1
+setenv hello 2
+setenv test
+setenv oui $PATH
+env
+unsetenv hello
+unsetenv hello
+env A=1
+./fixtures
+tests/
+|
+cat /dev/random | head -c 64 | sort | ohno
+|ls
+ls|
+ls -alr | sort | rev | head -n 5
+find . -maxdepth 3 -type f -name "*.c" | wc -l
+pwd; ls -li;;;;;;
+setenv
+setenv A. 2
+setenv 1 2
+setenv A B C
+ ls -l
+    uname   -a
+whoami
+ls  -d      /
+date
+unsetenv
+:
+builtins
+/root
+exit a
