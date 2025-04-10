66c213800f0c75e105610a7f67e77a4c01c2a042
Arthur-gtr <arthur.gauthier@epitech.eu>
2025-04-10 14:19:28 +0200

    [FIX] coding style

--- src/builtin_history.c
+++ src/builtin_history.c
@@ -32,51 +32,33 @@ static
 char *cat_in_str(his_variable_t *his_variable, char *str, char *cpy)
 {
     int i = 0;
-    int j = 0;
-    int x = his_variable->coord_variable;
     int len_str = strlen(str) - 1;//pour l \n
-    int size_right = len_str - x - his_variable->size_variable;
+    int size_right = len_str - his_variable->coord_variable - his_variable->size_variable;
     int size_left = (len_str - size_right) - his_variable->size_variable;  
     char *new_str = malloc(sizeof(char) * (size_right + size_left + strlen(cpy) + 2));//ajout de deux \0
-    //pas besoin de midlle car cpy existe 
 
-    printf("Size right: %d\nSize left: %d\nSize cpy: %d\nSize \\0: 2\n", size_right , size_left , strlen(cpy));
     if (new_str == NULL)
         return NULL;
     new_str[0] = 0;
-    for (; i < size_left; i++){
-        printf("left\n");
+    for (; i < size_left; i++)
         new_str[i] = str[i];
-    }
-    //printf("first loop; %s\n", new_str);
-    /*
-    **boucle for pour concatener cpy dans str
-    */
-    for (; cpy[j] != 0; j++){
-        
+    for (int j = 0; cpy[j] != 0; j++){
         new_str[i] = cpy[j]; 
         i++;
     }
-    printf("J-0; %d\n", j);
-    //j+= his_variable->size_variable;
-    printf("J-1; %d\n", j);
     for (int k = 0; k < size_right; k++){
-        printf("right\n");
         new_str[i] = str[k + size_left + his_variable->size_variable];
         i++;
-        j++;
     }
     new_str[i] = '\0';
     new_str[i + 1] = '\0'; 
-    printf("New str: %s\n", new_str);
-    free(new_str);
-    return NULL;
+    return new_str;
 }
 
 char *his_last_command(char *line, his_variable_t *his_variable, his_command_t *his_command)
 {
     char *new_line = malloc(sizeof(char) * 4);
-    //char *new_str = NULL;
+    char *new_str = NULL;
 
     if (new_line == NULL)
         return NULL;
@@ -84,10 +66,11 @@ char *his_last_command(char *line, his_variable_t *his_variable, his_command_t *
     new_line[1] = 's';
     new_line[2] = '\0';
     new_line[3] = '\0';
-    //cat_in_str(his_variable, line, new_line);
-    //printf("Adresse; %p && str; %s\n", new_line, new_line);
+    new_str = cat_in_str(his_variable, line, new_line);
+    printf("Adresse; %p && str; %s\n", new_line, new_line);
+    free(new_line);
     free(line);
-    return new_line;
+    return new_str;
 }
 
 char *his_last_same_command(char *line, his_variable_t *his_variable, his_command_t *his_command)
