d3c6319faa2787964f2dc306f21772221f8dbffd
savalet <savinien.petitjean@gmail.com>
2025-05-03 16:51:49 +0200

    Add if

--- src/shell.c
+++ src/shell.c
@@ -20,7 +20,6 @@
 #include "repl.h"
 #include "shell.h"
 #include "u_str.h"
-#include "visitor.h"
 
 __attribute__((unused))
 static
@@ -54,7 +53,7 @@ void write_prompt(int is_a_tty)
 static
 bool change_shell_command(buff_t *buff, exec_ctx_t *exec_ctx)
 {
-    char *tmp_buff = NULL;
+    char *tmp_buff = nullptr;
     size_t buffer_len;
 
     buff->sz = 0;
@@ -78,7 +77,7 @@ bool change_shell_command(buff_t *buff, exec_ctx_t *exec_ctx)
 static
 int shell_loop(int is_a_tty, exec_ctx_t *exec_ctx)
 {
-    buff_t buff = { .str = NULL, 0, .cap = BUFF_INIT_SZ };
+    buff_t buff = { .str = nullptr, 0, .cap = BUFF_INIT_SZ };
 
     init_shell_repl(exec_ctx);
     while (true) {
@@ -86,7 +85,6 @@ int shell_loop(int is_a_tty, exec_ctx_t *exec_ctx)
         if (!change_shell_command(&buff, exec_ctx))
             return exec_ctx->history->last_exit_code;
     }
-    free(exec_ctx->history_command);
     return free(buff.str), exec_ctx->history->last_exit_code;
 }
 
@@ -96,10 +94,10 @@ his_command_t *init_cmd_history(void)
     his_command_t *cmd_history = malloc(sizeof(his_command_t) * 100);
 
     if (cmd_history == NULL)
-        return NULL;
+        return nullptr;
     for (int i = 0; i != 100; i++){
-        cmd_history[i].arg = NULL;
-        cmd_history[i].command = NULL;
+        cmd_history[i].arg = nullptr;
+        cmd_history[i].command = nullptr;
         cmd_history[i].id = i;
     }
     cmd_history->sz = 0;
@@ -121,20 +119,19 @@ bool error_in_init(exec_ctx_t *exec_ctx)
     return false;
 }
 
-#include <signal.h>
 int shell(char **env_ptr)
 {
     alias_t alias = init_alias();
     env_t env = parse_env(env_ptr);
-    history_t history = { .cmd_history = NULL, .last_exit_code = 0,
-        .last_chdir = NULL};
+    history_t history = { .cmd_history = nullptr, .last_exit_code = 0,
+        .last_chdir = nullptr};
     his_command_t *cmd_history = init_cmd_history();
     local_t local = create_local();
     exec_ctx_t exec_ctx = {.env = &env, .local = &local,
         .history = &history, .history_command = cmd_history, .alias = &alias};
     int shell_result;
 
-    if (error_in_init(&exec_ctx) == true)
+    if (error_in_init(&exec_ctx))
         return RETURN_FAILURE;
     U_DEBUG_CALL(debug_env_entries, &env);
     shell_result = shell_loop(isatty(STDIN_FILENO), &exec_ctx);
