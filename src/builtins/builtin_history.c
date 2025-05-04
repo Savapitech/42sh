2f7b8cf28620099a4192400f424b66cc72cda2f0
savalet <savinien.petitjean@gmail.com>
2025-05-04 18:47:49 +0200

    Refactor history

--- src/builtins/builtin_history.c
+++ src/builtins/builtin_history.c
@@ -5,10 +5,14 @@
 ** builtin_history
 */
 
-#include <stdlib.h>
+#include <ctype.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "history.h"
+#include "utils.h"
 
 /*
 **Il faut deux \0 parce que dans le gettokeniser
@@ -26,31 +30,24 @@
 ** il vas free le buffer
 */
 
-#include <string.h>
-#include "utils.h"
-#include <ctype.h>
-#include <stddef.h>
-#include <stdlib.h>
-
 static char *concat_cmd_arg(char *dest, char *src)
 {
     int l;
     int i;
-    char *r_value = NULL;
+    char *r_value = nullptr;
 
     if (!src) {
         r_value = u_strdup(dest);
         return r_value;
-    } else {
-        l = strlen(dest);
-        i = strlen(src);
-        r_value = malloc(sizeof(char)* (i + l + 2));
-        if (r_value != NULL) {
-            strcpy(r_value, dest);
-            r_value[l] = ' ';
-            r_value[l +1] = '\0';
-            strcat(r_value, src);
-        }
+    }
+    l = strlen(dest);
+    i = strlen(src);
+    r_value = malloc(sizeof(char)* (i + l + 2));
+    if (r_value != NULL) {
+        strcpy(r_value, dest);
+        r_value[l] = ' ';
+        r_value[l +1] = '\0';
+        strcat(r_value, src);
     }
     return r_value;
 }
@@ -58,12 +55,12 @@ static char *concat_cmd_arg(char *dest, char *src)
 char *his_last_command(char *line,
     his_variable_t *his_variable, his_command_t *his_command)
 {
-    char *new_line = NULL;
-    char *new_str = NULL;
+    char *new_line = nullptr;
+    char *new_str = nullptr;
 
     if (his_command->sz == 0){
         fprintf(stderr, "%d: Event not found.\n", his_command->sz);
-        return NULL;
+        return nullptr;
     }
     new_line = concat_cmd_arg(his_command[his_command->sz - 1].command,
         his_command[his_command->sz - 1].arg);
@@ -76,7 +73,7 @@ char *his_last_same_command(char *line,
     his_variable_t *his_variable, his_command_t *his_command)
 {
     char *new_line = &line[his_variable->coord_variable + 1];
-    char *new_str = NULL;
+    char *new_str = nullptr;
 
     for (int i = his_command->sz - 1; i > 0; i--) {
         if (his_command[i].command == NULL) {
@@ -100,7 +97,7 @@ char *his_id_command(char *line,
 {
     int id = -1 + atoi(&line[his_variable->coord_variable + 1]);
     char *new_line;
-    char *new_str = NULL;
+    char *new_str = nullptr;
 
     if (id < 0 || id > 100 || his_command[id].command == NULL){
         fprintf(stderr, "%d: Event not found.\n", id + 1);
@@ -116,18 +113,18 @@ char *his_id_command(char *line,
 
 static char *get_last_word(char *str)
 {
-    char *last_word = NULL;
+    char *last_word = nullptr;
     int last_space = 0;
     int x = 0;
 
     if (!str)
-        return NULL;
+        return nullptr;
     while (str[x] != '\0') {
         if (isblank(str[x]))
             last_space = x + 1;
         x++;
     }
-    last_word = malloc(sizeof(char) * (x - last_space) + 1);
+    last_word = malloc(sizeof(char) * ((x - last_space) + 1));
     if (last_word != NULL) {
         last_word = strncpy(last_word, &str[last_space], x - last_space);
         last_word[x - last_space] = '\0';
@@ -138,15 +135,15 @@ static char *get_last_word(char *str)
 char *his_last_word(char *line,
     his_variable_t *his_variable, his_command_t *his_command)
 {
-    char *new_line = NULL;
-    char *new_str = NULL;
+    char *new_line = nullptr;
+    char *new_str = nullptr;
 
     if (his_command[his_command->sz - 1].arg == NULL){
         new_line = get_last_word(his_command[his_command->sz - 1].command);
     } else
         new_line = get_last_word(his_command[his_command->sz - 1].arg);
     if (!new_line)
-        return NULL;
+        return nullptr;
     new_str = cat_in_str(his_variable, line, new_line);
     printf("%s\n", new_str);
     free(new_line);
@@ -158,8 +155,8 @@ char *his_last_arg(char *line,
     his_variable_t *his_variable, his_command_t *his_command)
 {
     int id = his_command->sz - 1;
-    char *new_line = NULL;
-    char *new_str = NULL;
+    char *new_line = nullptr;
+    char *new_str = nullptr;
 
     if (!his_command[id].arg)
         new_line = " ";
